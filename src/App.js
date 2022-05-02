43a0afddcef4ace55b330f83a21c2f1731935071
PaulineJohansson <pauline89_johansson@hotmail.com>
2022-05-02 15:25:52 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Pauline Johansson and is{" "}
-        <a
-          href="https://github.com/PaulineJohansson/react-weather-app-project"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Pauline Johansson and is{" "}
+          <a
+            href="https://github.com/PaulineJohansson/react-weather-app-project"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
