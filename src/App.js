218ed2d0deace0d9ab4f00d111faa20bc1cf1c27
PaulineJohansson <pauline89_johansson@hotmail.com>
2022-05-02 14:33:08 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Pauline Johansson and is{" "}
+        <a
+          href="https://github.com/PaulineJohansson/react-weather-app-project"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
